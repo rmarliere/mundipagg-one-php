4a4c2efeedaf8d7bc86d774fcd0c158ec828c59f
Daniel Lima <daniel@docode.com.br>
2016-05-17 16:26:37 -0300

    New bins elo

--- tests/One/CreditCardHelperTest.php
+++ tests/One/CreditCardHelperTest.php
@@ -32,4 +32,29 @@ class CreditCardHelperTest extends \PHPUnit_Framework_TestCase
         $creditCard = CreditCardHelper::createCreditCard(" 5555 4444 3333 2222 ", " gateway ", " 12/2030 ", " 999 ");
         $this->assertEquals($expected, $creditCard);
     }
+
+    /**
+     * @dataProvider eloCreditCardNumbers
+     */
+    public function testCreateEloCreditCard_Success($creditCardNumber)
+    {
+        $creditCard = CreditCardHelper::createCreditCard($creditCardNumber, "FOO BAR", "10/2999", "123");
+        $this->assertEquals($creditCard->getCreditCardBrand(), CreditCardBrandEnum::ELO);
+    }
+
+    public function eloCreditCardNumbers()
+    {
+        return [
+            ["6500375776529289"],
+            ["6507175689691637"],
+            ["6507166749132449"],
+            ["6500352340085643"],
+            ["6500432908903922"],
+            ["6505381431696963"],
+            ["6500338068756598"],
+            ["5066991998066856"],
+            ["6362976992933317"],
+            ["4011793791559338"]
+        ];
+    }
 }
